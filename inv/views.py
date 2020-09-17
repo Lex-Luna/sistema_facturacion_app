f1ab49d8020fc95bc480b340521bd8433ed4cc08
geoku <geovannyc255@hotmail.com>
2020-09-16 23:19:00 -0500

    Creado el CRUD para subcategoria

--- inv/views.py
+++ inv/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse_lazy
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Categoria, SubCategoria
-from .forms import CategoriaForm
+from .forms import CategoriaForm, SubCategoriaForm
 # Create your views here.
  
 class CategoriaView(LoginRequiredMixin, generic.ListView):
@@ -51,5 +51,33 @@ class SubCategoriaView(LoginRequiredMixin, generic.ListView):
      context_object_name = "obj"
      login_url = 'bases:login'
 
+class SubCategoriaNew(LoginRequiredMixin, generic.CreateView):
+     model = SubCategoria
+     template_name = "inv/subcategoria_form.html"
+     context_object_name = "obj"
+     form_class=SubCategoriaForm
+     success_url = reverse_lazy("inv:subcategoria_list")
+     login_url = "bases:login"
 
+     def form_valid(self, form):
+          form.instance.uc = self.request.user
+          return super().form_valid(form)
+
+class SubCategoriaEdit(LoginRequiredMixin, generic.UpdateView):
+     model = SubCategoria
+     template_name = "inv/subcategoria_form.html"
+     context_object_name = "obj"
+     form_class = SubCategoriaForm
+     success_url = reverse_lazy("inv:subcategoria_list")
+     login_url = "bases:login"
+
+     def form_valid(self, form):
+          form.instance.um = self.request.user.id #uc usuario crea um usuario modifica
+          return super().form_valid(form)
+
+class SubCategoriaDel(LoginRequiredMixin, generic.DeleteView):
+     model = SubCategoria
+     template_name='inv/catalogos_del.html'
+     context_object_name = "obj"
+     success_url = reverse_lazy("inv:subcategoria_list")
 
