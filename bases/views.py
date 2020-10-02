333c998b7083ec5af1f59d4fb7a15d17fd2cbcb3
Lex <lex-luna@hotmail.com>
2020-10-01 19:24:15 -0500

    realizcion de la plantillla principal

--- bases/views.py
+++ bases/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 from django.views import generic 
+from django.urls import reverse_lazy
 
+class SinPrivilegios(LoginRequiredMixin, PermissionRequiredMixin):
+    raise_exception:False
+    redirect_field_name="redirect_to"
+    def handle_no_permission(self):
+        from django.contrib.auth.models import AnonymousUser
+        if not self.request.user==AnonymousUser(): 
+            self.login_url='bases:sin_privilegios'
+        return HttpResponseRedirect(reverse_lazy(self.login_url))
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
     login_url="bases:login"
 
 # Create your views here.
+class HomeSinPrivilegios(generic.TemplateView):
+    template_name= "bases/sin_privilegios.html"
---
+++ cmp/views.py
@@ -0,0 +1,66 @@
+  
+from django.shortcuts import render,redirect
+from django.views import generic
+from .models import Proveedor
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ProveedorForm
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+import json
+# Create your views here.
+class ProveedorView(LoginRequiredMixin, generic.ListView):
+    model = Proveedor
+    template_name = "cmp/proveedor_list.html"
+    context_object_name = "obj"
+    permission_required="cmp.view_proveedor"
+
+class ProveedorNew(LoginRequiredMixin, generic.CreateView):
+    model=Proveedor
+    template_name="cmp/proveedor_form.html"
+    context_object_name = 'obj'
+    form_class= ProveedorForm
+    success_url= reverse_lazy("cmp:proveedor_list")
+    success_message="Proveedor Nuevo"
+    permission_required="cmp.add_proveedor"
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        #print(self.request.user.id)
+        return super().form_valid(form)
+
+
+class ProveedorEdit(LoginRequiredMixin, generic.UpdateView):
+    model=Proveedor
+    template_name="cmp/proveedor_form.html"
+    context_object_name = 'obj'
+    form_class=ProveedorForm
+    success_url= reverse_lazy("cmp:proveedor_list")
+    success_message="Proveedor Editado"
+    permission_required="cmp.change_proveedor"
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        print(self.request.user.id)
+        return super().form_valid(form)
+
+
+# @login_required(login_url="/login/")
+# @permission_required("cmp.change_proveedor",login_url="/login/")
+def proveedorInactivar(request,id):
+    template_name='cmp/inactivar_prv.html'
+    contexto={}
+    prv = Proveedor.objects.filter(pk=id).first()
+
+    if not prv:
+        return HttpResponse('Proveedor no existe ' + str(id))
+
+    if request.method=='GET':
+        contexto={'obj':prv}
+
+    if request.method=='POST':
+        prv.estado=False
+        prv.save()
+        contexto={'obj':'OK'}
+        return HttpResponse('Proveedor Inactivado')
+
+    return render(request,template_name,contexto)
