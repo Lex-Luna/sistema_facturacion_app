b6d589db4d16a135d64cca8716ae80045986351f
Lex <lex-luna@hotmail.com>
2020-10-07 23:24:43 -0500

    terminada la aplicacion cmp y creada la app factura

--- cmp/urls.py
+++ cmp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from .views import ProveedorView,ProveedorNew, ProveedorEdit, proveedorInactivar
+from .views import ProveedorView,ProveedorNew, ProveedorEdit, proveedorInactivar, ComprasView, compras,ComprasView, compras, CompraDetDelete
     # , \
     # ComprasView, compras, CompraDetDelete
 
-# from .reportes import reporte_compras, imprimir_compra
+from .reportes import reporte_compras, imprimir_compra
 
 urlpatterns = [
     path('proveedores/',ProveedorView.as_view(), name="proveedor_list"),
@@ -12,11 +12,11 @@ urlpatterns = [
     path('proveedores/edit/<int:pk>',ProveedorEdit.as_view(), name="proveedor_edit"),
     path('proveedores/inactivar/<int:id>',proveedorInactivar, name="proveedor_inactivar"),
 
-    # path('compras/',ComprasView.as_view(), name="compras_list"),
-    # path('compras/new',compras, name="compras_new"),
-    # path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
-    # path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
+    path('compras/',ComprasView.as_view(), name="compras_list"),
+    path('compras/new',compras, name="compras_new"),
+    path('compras/edit/<int:compra_id>',compras, name="compras_edit"),
+    path('compras/<int:compra_id>/delete/<int:pk>',CompraDetDelete.as_view(), name="compras_del"),
 
-    # path('compras/listado', reporte_compras, name='compras_print_all'),
-    # path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
+    path('compras/listado', reporte_compras, name='compras_print_all'),
+    path('compras/<int:compra_id>/imprimir', imprimir_compra,name="compras_print_one"),
 ]
---
+++ fac/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+#Para los signals
+# from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
+# from django.db.models import Sum
+
+from bases.models import ClaseModelo #ClaseModelo2
+# from inv.models import Producto
+
+class Cliente(ClaseModelo):
+    NAT='Natural'
+    JUR='Jurídica'
+    TIPO_CLIENTE = [
+        (NAT,'Natural'),
+        (JUR,'Jurídica')
+    ]
+    nombres = models.CharField(
+        max_length=100
+    )
+    apellidos = models.CharField(
+        max_length=100
+    )
+    celular = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True
+    )
+    tipo=models.CharField(
+        max_length=10,
+        choices=TIPO_CLIENTE,
+        default=NAT
+    )
+
+    def __str__(self):
+        return '{} {}'.format(self.apellidos,self.nombres)
+
+    def save(self):
+        self.nombres = self.nombres.upper()
+        self.apellidos = self.apellidos.upper()
+        super(Cliente, self).save()
+
+    class Meta:
+        verbose_name_plural = "Clientes"
